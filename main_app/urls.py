25498460e0693cfb8dffc35a5b32d93238f15084
sayumadegita <sayumadegitavaluvi@gmail.com>
2026-04-05 15:06:14 +0700

    Lab Session 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add/', views.add_report, name='add_report'),
+    path('edit/<int:pk>/', views.edit_report, name='edit_report'),
+    path('delete/<int:pk>/', views.delete_report, name='delete_report'),
 ]
