07c8441f1b6147d7cbac4d182ba1ba09c887ba0e
sayumadegita <sayumadegitavaluvi@gmail.com>
2026-06-13 16:11:26 +0700

    Deploy frontend SPA to GitHub Pages

--- smartcity_citizen_spa_24782030/js/api.js
+++ smartcity_citizen_spa_24782030/js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'http://103.151.63.84:8010';
 
 async function requestAPI(endpoint, method = 'GET', bodyData = null) {
     const accessToken = localStorage.getItem('access');
